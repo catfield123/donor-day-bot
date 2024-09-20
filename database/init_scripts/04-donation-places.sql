05c43d67760c5f3bfe6bb3d0cb270e92343da1cb
catfield123 <gtagta65@gmail.com>
2024-09-20 16:22:26 +0300

    tables for donation place and datetime

---
+++ database/init_scripts/04-donation-places.sql
@@ -0,0 +1,16 @@
+CREATE TABLE donation_place (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL UNIQUE,
+);
+
+INSERT INTO donation_place VALUES 
+    ("Башня Политеха"),
+    ("Елизаветинская больница")
+;
+
+CREATE TABLE donation_datetime (
+    place_id INTEGER NOT NULL REFERENCES donation_place(id),
+    datetime TIMESTAMP NOT NULL,
+
+    PRIMARY KEY (place_id, datetime)
+)
