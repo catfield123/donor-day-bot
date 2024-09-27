fbf569d44b0cc3b947ff6aa014c98b8e9e729454
catfield123 <gtagta65@gmail.com>
2024-09-28 02:56:00 +0300

    handle errors + escape name string:
    Handled exceptions:
    - not admin exception
    - not volunteer exception
    - database connection error

--- staff-bot/src/main.py
+++ staff-bot/src/main.py
@@ -5,12 +5,21 @@ import sys
 from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
-from aiogram.filters import CommandStart
+from aiogram.filters import CommandStart, ExceptionTypeFilter
 from aiogram.types import Message
+from aiogram import F
+from aiogram.types.error_event import ErrorEvent
 
 from aiogram.fsm.context import FSMContext
 
+from common.exceptions import DatabaseConnectionError
 from common.states import IdleStates
+from common.keyboards import remove_keyboard
+from common.utils import escape_markdown_v2
+from common.responses import BOT_IS_UNAVAILABLE_RESPONSE
+
+from exceptions.auth import NotAdminException, NotVolunteerException
+from responses.auth import AuthResponses
 
 from config import staff_settings
 TOKEN = staff_settings.STAFF_BOT_TOKEN
@@ -24,11 +33,23 @@ from handlers.help_and_my_status import help_and_status_router
 dp.include_router(new_volunteer_router)
 dp.include_router(help_and_status_router)
 
+@dp.error(ExceptionTypeFilter(NotAdminException), F.update.message.as_("message"))
+async def handle_not_admin_exception(event: ErrorEvent, message: Message):
+    await message.answer(AuthResponses.NOT_ADMIN_RESPONSE, reply_markup=remove_keyboard)
+
+@dp.error(ExceptionTypeFilter(NotVolunteerException), F.update.message.as_("message"))
+async def handle_not_volunteer_exception(event: ErrorEvent, message: Message):
+    await message.answer(AuthResponses.NOT_VOLUNTEER_RESPONSE, reply_markup=remove_keyboard)
+
+@dp.error(ExceptionTypeFilter(DatabaseConnectionError), F.update.message.as_("message"))
+async def handle_database_connection_error(event: ErrorEvent, message: Message):
+    await message.answer(BOT_IS_UNAVAILABLE_RESPONSE, reply_markup=remove_keyboard)
+
 @dp.message(CommandStart())
 async def command_start_handler(message: Message, state: FSMContext) -> None:
     current_state = await state.get_state()
     if current_state is None:
-        await message.answer(f"Hello, {message.from_user.full_name}\!")
+        await message.answer(f"Hello, {escape_markdown_v2(message.from_user.full_name)}\!")
         await state.set_state(IdleStates.idle)
 
 
