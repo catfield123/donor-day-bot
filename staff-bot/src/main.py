8a4fdca9f8f4d294b15caeaac27bdd64f59e9100
catfield123 <gtagta65@gmail.com>
2024-09-27 16:35:10 +0300

    unused imports removed

--- staff-bot/src/main.py
+++ staff-bot/src/main.py
@@ -1,9 +1,8 @@
 import asyncio
 import logging
 import sys
-from os import getenv
 
-from aiogram import Bot, Dispatcher, html
+from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from aiogram.filters import CommandStart
