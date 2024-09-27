6e00a9596547229c646fc1d2e9ac2af809feef50
catfield123 <gtagta65@gmail.com>
2024-09-28 02:44:31 +0300

    raise DatabaseConnectionError if something went wrong

---
+++ common/exceptions.py
@@ -0,0 +1,3 @@
+
+class DatabaseConnectionError(Exception):
+    pass
--- common/middleware.py
+++ common/middleware.py
@@ -1,6 +1,8 @@
 from typing import Any, Awaitable, Callable
 from aiogram import BaseMiddleware, types
 from common.db import SessionLocal
+from sqlalchemy.exc import SQLAlchemyError
+from common.exceptions import DatabaseConnectionError
 
 class DatabaseMiddleware(BaseMiddleware):
     def __init__(self) -> None:
@@ -12,9 +14,11 @@ class DatabaseMiddleware(BaseMiddleware):
         event: types.Message,
         data: dict
     ) -> Any:
-        data['db'] = SessionLocal()
         try:
+            data['db'] = SessionLocal()
             return await handler(event, data)
+        except SQLAlchemyError as e:
+            raise DatabaseConnectionError("Error while connecting to database") from e
         finally:
             db = data.get('db')
             if db:
