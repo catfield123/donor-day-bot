83631bf3e1a29c66bdbec98c78c890a4cd07d57b
catfield123 <gtagta65@gmail.com>
2024-09-28 02:49:58 +0300

    type cast telegram_id

--- staff-bot/src/schemas.py
+++ staff-bot/src/schemas.py
@@ -12,10 +12,16 @@ class NewVolunteerRequestSchema(BaseModel):
     telegram_id : str
     granted_by_telegram_id : str
 
+    model_config = ConfigDict(coerce_numbers_to_str=True)
+
 class ChangeDonorStatusRequestSchema(BaseModel):
     id : int
     donor_status : DonorStatusEnum
 
+    model_config = ConfigDict(coerce_numbers_to_str=True)
+
 class ShowDonorInfoToVolunteerSchema(UserBase):
     id : int
     donor_status : DonorStatusEnum
+    
+    
