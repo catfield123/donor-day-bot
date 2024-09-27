5756e0d0dc3502c6a815ba7731d6d35da2241457
catfield123 <gtagta65@gmail.com>
2024-09-27 21:57:47 +0300

    idle state after /start

--- staff-bot/src/main.py
+++ staff-bot/src/main.py
@@ -8,6 +8,10 @@ from aiogram.enums import ParseMode
 from aiogram.filters import CommandStart
 from aiogram.types import Message
 
+from aiogram.fsm.context import FSMContext
+
+from common.states import IdleStates
+
 from config import staff_settings
 TOKEN = staff_settings.STAFF_BOT_TOKEN
 
@@ -19,8 +23,11 @@ from handlers.new_volunteer import new_volunteer_router
 dp.include_router(new_volunteer_router)
 
 @dp.message(CommandStart())
-async def command_start_handler(message: Message) -> None:
-    await message.answer(f"Hello, {message.from_user.full_name}!")
+async def command_start_handler(message: Message, state: FSMContext) -> None:
+    current_state = await state.get_state()
+    if current_state is None:
+        await message.answer(f"Hello, {message.from_user.full_name}\!")
+        await state.set_state(IdleStates.idle)
 
 
 async def main() -> None:
