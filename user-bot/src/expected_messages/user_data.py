80384224a20bd1f05853f3ba40ae96e6fc7f27bb
catfield123 <gtagta65@gmail.com>
2024-09-29 03:21:52 +0300

    expected messages for user_data procedure

---
+++ user-bot/src/expected_messages/user_data.py
@@ -0,0 +1,45 @@
+class UserDataExpectedMessages:
+    ENTER_DATA = "Начать ввод данных"
+
+    CONFIRM_ENTERED_DATA = "Да"
+    REENTER_DATA = "Ввести заново"
+    
+    YES_IS_POLYTECH_STUDENT = "Я студент Политеха"
+    NO_IS_POLYTECH_STUDENT = "Я не студент Политеха"
+
+    I_HAVE_NO_PATRONYMIC = "У меня нет отчества"
+
+    I_HAVE_NO_INN = "У меня нет ИНН"
+
+    I_HAVE_NO_SNILS = "У меня нет СНИЛС"
+
+    BUDGET_FOUNDING_SOURCE = "Бюджет"
+    CONTRACT_FOUNDING_SOURCE = "Контракт"
+
+    I_DONT_REMEMBER_MY_GRADE_BOOK_NUMBER = "Я не помню номер зачётной книжки"
+
+    I_AGREE_FOR_BONE_MARROW_TYPING = "Согласен на типирование"
+    I_DECLINE_FOR_BONE_MARROW_TYPING = "Не согласен на типирование"
+
+    ALL_DATA_IS_CORRECT = "Все данные верны"
+
+    EDIT_FULL_NAME = "Изменить ФИО"
+    EDIT_PHONE_NUMBER = "Изменить номер телефона"
+    EDIT_EMAIL = "Изменить почту"
+    EDIT_STUDYING_PLACE_INFO = "Изменить информацию о месте обучения"
+    EDIT_INN = "Изменить ИНН"
+    EDIT_SNILS = "Изменить СНИЛС"
+
+    EDIT_PASSPORT_DATA = "Изменить данные паспорта"
+    EDIT_SEX_DATA = "Изменить данные о пол"
+    EDIT_WEIGHT_DATA = "Изменить данные о весе"
+    EDIT_BONE_MARROW_TYPING_AGREEMENT_DATA = "Изменить согласие на типирование"
+
+    EDIT_DONATION_PLACE_AND_DATETIME = "Изменить место и время сдачи крови"
+
+    CANCEL_OPERATION = "Отменить операцию"
+
+    EDIT_DATA = "Изменить данные"
+
+
+
