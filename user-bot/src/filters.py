ddc813c89288cdde758df85fcb466b7c2e2b4880
catfield123 <gtagta65@gmail.com>
2024-09-29 06:06:44 +0300

    created custom filters for allowed answers, confirmation and re-entering data

---
+++ user-bot/src/filters.py
@@ -0,0 +1,21 @@
+from aiogram.filters import Filter
+from aiogram.types import Message
+
+from expected_messages.user_data import UserDataExpectedMessages
+
+
+class AllowedAnswers(Filter):
+    def __init__(self, allowed_answers_list: list[str]) -> None:
+        self.allowed_answers_list = allowed_answers_list
+
+    async def __call__(self, message: Message) -> bool:
+        return message.text in self.allowed_answers_list
+    
+
+class ConfirmEnteredData(Filter):
+    async def __call__(self, message: Message) -> bool:
+        return message.text == UserDataExpectedMessages.CONFIRM_ENTERED_DATA
+
+class ReenterData(Filter):
+    async def __call__(self, message: Message) -> bool:
+        return message.text == UserDataExpectedMessages.REENTER_DATA
