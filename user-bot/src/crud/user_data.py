ea3b96e2bc0051577923e4e89335fc2a796003bf
catfield123 <gtagta65@gmail.com>
2024-09-30 21:59:57 +0300

    add getters of donation place and donation datetimes from db

--- user-bot/src/crud/user_data.py
+++ user-bot/src/crud/user_data.py
@@ -1,6 +1,22 @@
 
 from sqlalchemy.orm import Session
-from common.models import Faculty
+from common.models import DonationDatetime, Faculty, DonationPlace
 
+from schemas import DonationDatetimesDbResponse, DonationPlaceDbResponse
+
+from functools import lru_cache
+
+@lru_cache
 def db_get_faculties_names(db: Session):
     return [faculty.name for faculty in db.query(Faculty).all()]
+
+@lru_cache
+def db_get_donation_places(db: Session) -> list[DonationPlaceDbResponse]:
+    return [DonationPlaceDbResponse(id=donation_place.id, name=donation_place.name) for donation_place in db.query(DonationPlace).all()]
+
+@lru_cache
+def db_get_donation_datetimes(db: Session, donation_place_id: int) -> list[DonationDatetimesDbResponse]:
+    return [
+        DonationDatetimesDbResponse(id=donation_datetime.id, place_id = donation_datetime.place_id,datetime=donation_datetime.datetime) 
+        for donation_datetime in db.query(DonationDatetime).filter(DonationDatetime.place_id == donation_place_id).all()
+        ]
