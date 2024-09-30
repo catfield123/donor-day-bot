ec395703792aec422143bbd14568a6d26f00cdb8
catfield123 <gtagta65@gmail.com>
2024-09-30 22:01:10 +0300

    inline keyboards for choosing place and datetime

--- user-bot/src/keyboards/inline/user_data.py
+++ user-bot/src/keyboards/inline/user_data.py
@@ -1,6 +1,11 @@
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from expected_messages.user_data import UserDataExpectedMessages
 
+from sqlalchemy.orm import Session
+from crud.user_data import db_get_donation_datetimes, db_get_donation_places
+
+from callbacks import ChooseDonationDatetimeCallback, ChooseDonationPlaceCallback
+
 class UserDataInlineKeyboard:
     edit_data_keyboard = InlineKeyboardMarkup(
         inline_keyboard=[
@@ -28,9 +33,47 @@ class UserDataInlineKeyboard:
             ],
             [
                 InlineKeyboardButton(text=UserDataExpectedMessages.EDIT_DONATION_PLACE_AND_DATETIME, callback_data='edit_donation_place_and_datetime'),
-                InlineKeyboardButton(text=UserDataExpectedMessages.CANCEL_OPERATION, callback_data='cancel_operation')
             ]
         ]
     )
+
+
+    @staticmethod
+    def get_choose_donation_place_keyboard(db: Session) -> InlineKeyboardMarkup:
+        donation_places = db_get_donation_places(db)
+
+        keyboard_buttons = []
+        for donation_place in donation_places:
+            keyboard_buttons.append([InlineKeyboardButton(text=donation_place.name, 
+                                                            callback_data=ChooseDonationPlaceCallback(
+                                                                id=f"{donation_place.id}",
+                                                                name=donation_place.name
+                                                                ).pack()
+                                                        )   
+            ])
+        
+        return InlineKeyboardMarkup(
+                inline_keyboard=keyboard_buttons
+            )
+    
+
+    @staticmethod
+    def get_choose_donation_datetime_keyboard(db: Session, donation_place_id: int) -> InlineKeyboardMarkup:
+        donation_datetimes = db_get_donation_datetimes(db, donation_place_id)
+
+        keyboard_buttons = []
+        for donation_datetime in donation_datetimes:
+            keyboard_buttons.append([InlineKeyboardButton(text=donation_datetime.datetime, 
+                                                            callback_data=ChooseDonationDatetimeCallback(
+                                                                id=f"{donation_datetime.id}",
+                                                                place_id=f"{donation_datetime.place_id}",
+                                                                datetime=donation_datetime.datetime
+                                                                ).pack()
+                                                        )   
+            ])
+        
+        return InlineKeyboardMarkup(
+                inline_keyboard=keyboard_buttons
+            )
 
     
