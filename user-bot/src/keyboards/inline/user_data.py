6affcf3371b2da1b7cf3a7f210af0217d4ee9d4b
catfield123 <gtagta65@gmail.com>
2024-10-01 16:09:09 +0300

    use custom callbacks in edit_data_keyboard

--- user-bot/src/keyboards/inline/user_data.py
+++ user-bot/src/keyboards/inline/user_data.py
@@ -4,35 +4,35 @@ from expected_messages.user_data import UserDataExpectedMessages
 from sqlalchemy.orm import Session
 from crud.user_data import db_get_donation_datetimes, db_get_donation_places
 
-from callbacks import ChooseDonationDatetimeCallback, ChooseDonationPlaceCallback
+from callbacks import AllDataIsCorrectCallback, ChooseDonationDatetimeCallback, ChooseDonationPlaceCallback, EditDataCallback
 
 class UserDataInlineKeyboard:
     edit_data_keyboard = InlineKeyboardMarkup(
         inline_keyboard=[
-            [InlineKeyboardButton(text=UserDataExpectedMessages.ALL_DATA_IS_CORRECT, callback_data='all_data_is_correct')],
+            [InlineKeyboardButton(text=UserDataExpectedMessages.ALL_DATA_IS_CORRECT, callback_data=AllDataIsCorrectCallback().pack())],
             
             [
-                InlineKeyboardButton(text=UserDataExpectedMessages.EDIT_FULL_NAME, callback_data='edit_full_name'),
-                InlineKeyboardButton(text=UserDataExpectedMessages.EDIT_PHONE_NUMBER, callback_data='edit_phone_number')
+                InlineKeyboardButton(text=UserDataExpectedMessages.EDIT_FULL_NAME, callback_data=EditDataCallback(fields='full_name').pack()),
+                InlineKeyboardButton(text=UserDataExpectedMessages.EDIT_PHONE_NUMBER, callback_data=EditDataCallback(fields='phone_number').pack())
             ],
             [
-                InlineKeyboardButton(text=UserDataExpectedMessages.EDIT_EMAIL, callback_data='edit_email'),
-                InlineKeyboardButton(text=UserDataExpectedMessages.EDIT_STUDYING_PLACE_INFO, callback_data='edit_studying_place_info')
+                InlineKeyboardButton(text=UserDataExpectedMessages.EDIT_EMAIL, callback_data=EditDataCallback(fields='email').pack()),
+                InlineKeyboardButton(text=UserDataExpectedMessages.EDIT_STUDYING_PLACE_INFO, callback_data=EditDataCallback(fields='studying_place_info').pack())
             ],
             [
-                InlineKeyboardButton(text=UserDataExpectedMessages.EDIT_INN, callback_data='edit_inn'),
-                InlineKeyboardButton(text=UserDataExpectedMessages.EDIT_SNILS, callback_data='edit_snils')
+                InlineKeyboardButton(text=UserDataExpectedMessages.EDIT_INN, callback_data=EditDataCallback(fields='inn').pack()),
+                InlineKeyboardButton(text=UserDataExpectedMessages.EDIT_SNILS, callback_data=EditDataCallback(fields='snils').pack())
             ],
             [
-                InlineKeyboardButton(text=UserDataExpectedMessages.EDIT_PASSPORT_DATA, callback_data='edit_passport_data'),
-                InlineKeyboardButton(text=UserDataExpectedMessages.EDIT_SEX_DATA, callback_data='edit_sex_data')
+                InlineKeyboardButton(text=UserDataExpectedMessages.EDIT_PASSPORT_DATA, callback_data=EditDataCallback(fields='passport_data').pack()),
+                InlineKeyboardButton(text=UserDataExpectedMessages.EDIT_SEX_DATA, callback_data=EditDataCallback(fields='sex').pack())
             ],
             [
-                InlineKeyboardButton(text=UserDataExpectedMessages.EDIT_WEIGHT_DATA, callback_data='edit_weight_data'),
-                InlineKeyboardButton(text=UserDataExpectedMessages.EDIT_BONE_MARROW_TYPING_AGREEMENT_DATA, callback_data='edit_bone_marrow_typing_agreement_data')
+                InlineKeyboardButton(text=UserDataExpectedMessages.EDIT_WEIGHT_DATA, callback_data=EditDataCallback(fields='weight').pack()),
+                InlineKeyboardButton(text=UserDataExpectedMessages.EDIT_BONE_MARROW_TYPING_AGREEMENT_DATA, callback_data=EditDataCallback(fields='bone_marrow_typing_agreement').pack())
             ],
             [
-                InlineKeyboardButton(text=UserDataExpectedMessages.EDIT_DONATION_PLACE_AND_DATETIME, callback_data='edit_donation_place_and_datetime'),
+                InlineKeyboardButton(text=UserDataExpectedMessages.EDIT_DONATION_PLACE_AND_DATETIME, callback_data=EditDataCallback(fields='donation_place_and_datetime').pack()),
             ]
         ]
     )
