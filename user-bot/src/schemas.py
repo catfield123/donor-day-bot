8052c98f4acc40f32b2ec4b5323fdf00a259be8d
catfield123 <gtagta65@gmail.com>
2024-10-01 17:35:13 +0300

    Schema for showing data to user for recheck

--- user-bot/src/schemas.py
+++ user-bot/src/schemas.py
@@ -55,3 +55,50 @@ class DonationDatetimesDbResponse(BaseModel):
     @field_validator("datetime", mode="before")
     def validate_datetime(cls, value):
         return value.strftime("%d.%m.%Y %H:%M")
+
+
+class UserRecheckData(BaseModel):
+    name: str
+    surname: str
+    patronymic: str | None = None
+    @property
+    def full_name(self) -> str:
+        if self.patronymic:
+            return f"{self.surname} {self.name} {self.patronymic}"
+        return f"{self.surname} {self.name}"
+
+    phone_number: str
+    email: str
+
+    is_polytech_student: bool
+    grade_book_number: str | None
+    group_number: str | None
+    faculty: str | None
+    founding_source: FoundingSourceEnum | None
+
+    inn: str
+    snils: str
+
+    passport_series: str
+    passport_number: str
+    @property
+    def passport_full_number(self) -> str:
+        return f"{self.passport_series} {self.passport_number}"
+
+    passport_issued_by: str
+    passport_issued_date: str
+    passport_issued_organization_code: str
+    @property
+    def passport_issued_by_full_text(self) -> str:
+        return f"{self.passport_issued_by} {self.passport_issued_date}, код организации {self.passport_issued_organization_code}"
+
+    birth_date: str
+    birth_place: str
+    registration_address: str
+
+    sex: SexEnum
+    body_weight: BodyWeightEnum
+    bone_marrow_typing_agreement: bool
+
+    donation_place: str
+    donation_datetime: str
