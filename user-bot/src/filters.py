93b9c2d3ca4f6106eb1bcf37395a9160205c93a6
catfield123 <gtagta65@gmail.com>
2024-10-01 16:17:48 +0300

    changed argument type: list[str] -> list[str] | str

--- user-bot/src/filters.py
+++ user-bot/src/filters.py
@@ -5,8 +5,13 @@ from expected_messages.user_data import UserDataExpectedMessages
 
 
 class AllowedAnswers(Filter):
-    def __init__(self, allowed_answers_list: list[str]) -> None:
-        self.allowed_answers_list = allowed_answers_list
+    def __init__(self, allowed_answers: list[str] | str) -> None:
+        if isinstance(allowed_answers, str):
+            self.allowed_answers_list = [allowed_answers]
+        elif isinstance(allowed_answers, list):
+            self.allowed_answers_list = allowed_answers
+        else:
+            raise TypeError(f"allowed_answers must be list[str] or str, not {type(allowed_answers)}")
 
     async def __call__(self, message: Message) -> bool:
         return message.text in self.allowed_answers_list
